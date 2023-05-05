15509f35db9eb6feeb8e994a13e6f0f5cd6cb13c
boluwatife19 <114581776+boluwatife19@users.noreply.github.com>
2023-05-06 03:37:44 +0500

    Add files via upload

---
+++ Day 5/script.js
@@ -0,0 +1,144 @@
+const paragraph = [
+    `The human brain is a remarkable organ. It is responsible for everything we think, feel, and do. Despite years of research, there is still so much we don't know about this complex and fascinating structure. Scientists estimate that the average adult brain contains around 100 billion neurons, each connected to thousands of others, forming a vast network that controls everything from breathing to decision making. Can you type this passage without making any errors?`,
+`The world is full of amazing places, from the towering peaks of the Himalayas to the lush rainforests of the Amazon. Each location has its unique beauty, history, and culture. For many people, travel is the best way to experience these wonders and broaden their horizons. However, travel can also be challenging, with language barriers, cultural differences, and unfamiliar customs to navigate. Can you type this passage about the joys and difficulties of travel without any mistakes?`,
+`Writing is a powerful form of communication. It allows us to express our thoughts and feelings, share our experiences, and connect with others. Whether you're writing a novel, an essay, or an email, it's important to communicate your message clearly and effectively. This requires a combination of creativity, skill, and practice. Can you type this passage about the importance of writing without any typos?`,
+`The internet has revolutionized the way we live and work. It has connected us to people and information from all around the world, providing endless opportunities for learning and growth. However, the internet has also created new challenges, such as cyberbullying, online scams, and addiction. As we navigate this digital landscape, it's essential to stay informed and use the internet responsibly. Can you type this passage about the benefits and risks of the internet without making any mistakes?`,
+`Education is the key to success. It provides us with the knowledge and skills we need to achieve our goals and make a positive impact on the world. However, education is not just about memorizing facts and passing exams. It's about developing critical thinking, creativity, and empathy. It's about learning how to learn, adapting to new challenges, and collaborating with others. Can you type this passage about the true meaning of education without any errors?`,
+`Music is a universal language. It has the power to evoke emotions, unite people, and transcend cultural barriers. Whether you're playing an instrument, singing, or simply listening, music can bring joy, inspiration, and healing. It's no wonder that music has been an integral part of human culture for thousands of years. Can you type this passage about the significance of music without making any errors?`,
+`Health is wealth. Without good health, we cannot enjoy life to its fullest or pursue our dreams. That's why it's essential to take care of our physical and mental well-being. This includes eating a healthy diet, exercising regularly, getting enough sleep, and managing stress. It also means seeking medical attention when necessary and practicing self-care. Can you type this passage about the importance of health without any typos?`,
+`Climate change is one of the greatest challenges facing our planet. It is causing rising temperatures, more frequent natural disasters, and the extinction of many species. The good news is that we can all make a difference by taking action to reduce our carbon footprint. This can include using renewable energy, conserving resources, and advocating for policies that support sustainability. Can you type this passage about the urgent need to address climate change without making any mistakes?`,
+`Success means different things to different people. For some, it's about achieving financial security or fame. For others, it's about making a positive impact on the world or living a fulfilling life. Whatever your definition of success may be, it's important to set clear goals, work hard, and persevere in the face of challenges. Can you type this passage about the nature of success without any errors?`,
+`Friendship is one of life's greatest treasures. It is a bond built on trust, mutual respect, and shared experiences. Friends are there to support us through the good times and the bad, to laugh with us, and to offer a shoulder to cry on. To cultivate strong friendships, it's important to be a good listener, show empathy, and be reliable. Can you type this passage about the value of friendship without any typos?`,
+`Art is a form of expression that transcends language and culture. It can inspire, challenge, and move us. Whether you're creating art or experiencing it, it's a powerful way to connect with yourself and others. From cave paintings to contemporary installations, art has been an integral part of human history. Can you type this passage about the significance of art without making any errors?`,
+`The world is full of diversity, from the myriad of cultures to the vast array of flora and fauna. This diversity is what makes our planet so rich and vibrant. However, it's also important to remember that we all share a common humanity. We all have dreams, hopes, and fears. By embracing our differences and celebrating our similarities, we can create a more peaceful and inclusive world. Can you type this passage about the importance of diversity without any typos?`,
+`The human body is a complex and amazing machine. It can heal itself, fight off diseases, and perform incredible feats of strength and endurance. To keep our bodies healthy, we need to eat a balanced diet, exercise regularly, and get enough rest. We also need to pay attention to our mental health, practicing self-care and seeking support when necessary. Can you type this passage about the marvels of the human body without making any mistakes?`,
+`Traveling is one of the most enriching experiences life has to offer. It allows us to see new places, meet new people, and learn about different cultures. However, traveling can also be expensive and time-consuming. That's why it's important to plan ahead, budget wisely, and prioritize the experiences that matter most to you. Can you type this passage about the joys and challenges of travel without any errors?`,
+`Technology has transformed our world in countless ways. It has made communication faster and easier, provided new ways to work and learn, and given us access to an endless stream of information. However, it's also important to be aware of the potential negative impacts of technology, such as addiction, social isolation, and privacy concerns. Can you type this passage about the benefits and drawbacks of technology without making any mistakes?`,
+`Education is a lifelong journey. It doesn't end when we graduate from school or university. To stay relevant and adapt to new challenges, we need to keep learning and growing. This can include reading books, taking courses, attending conferences, and seeking mentorship. By embracing a growth mindset and investing in our education, we can unlock new opportunities and achieve our goals. Can you type this passage about the importance of lifelong learning without any errors?`,
+`Education is a lifelong journey. It doesn't end when we graduate from school or university. To stay relevant and adapt to new challenges, we need to keep learning and growing. This can include reading books, taking courses, attending conferences, and seeking mentorship. By embracing a growth mindset and investing in our education, we can unlock new opportunities and achieve our goals. Can you type this passage about the importance of lifelong learning without any errors?`,
+`Happiness is a state of mind. It's not something that can be bought or achieved through external means. Rather, it's an internal state of peace, contentment, and joy. To cultivate happiness, we need to focus on the things that truly matter, such as our relationships, personal growth, and sense of purpose. This can involve practicing gratitude, mindfulness, and self-care. By prioritizing our own happiness, we can also inspire others to do the same and create a more positive and fulfilling world. Can you type this passage about the nature of happiness without making any mistakes?`,
+`Music is a universal language that speaks to the soul. It can evoke powerful emotions, unite communities, and provide a soundtrack to our lives. Whether you're listening to classical symphonies or contemporary pop songs, music has the power to move us in profound ways. It's also a creative outlet for many people, allowing them to express themselves and connect with others through shared experiences. Can you type this passage about the significance of music without any errors?`,
+`Time is our most valuable resource. It's finite, and once it's gone, we can never get it back. That's why it's important to use our time wisely and intentionally. This can involve setting goals, prioritizing tasks, and minimizing distractions. It's also important to make time for the things that truly matter, such as spending time with loved ones and pursuing our passions. By using our time effectively, we can create a more fulfilling and purposeful life. Can you type this passage about the importance of time management without any typos?`,
+`Leadership is the ability to inspire and influence others towards a common goal. It involves taking responsibility, making tough decisions, and fostering a culture of collaboration and innovation. However, leadership is not just reserved for those in formal positions of power. Anyone can be a leader by setting an example, providing mentorship, and empowering others. By cultivating our leadership skills, we can make a positive impact on the world around us. Can you type this passage about the qualities of effective leadership without any mistakes?`,
+`Food is not just fuel for our bodies, but also a source of pleasure, culture, and community. It has the power to bring people together, create memories, and tell stories. To enjoy food to its fullest, we need to savor each bite, appreciate the ingredients and flavors, and pay attention to the way it makes us feel. It's also important to make healthy and sustainable food choices, both for our own well-being and that of the planet. Can you type this passage about the significance of food without making any errors?`,
+`Language is a tool for communication, but it's also much more than that. It shapes our perceptions of the world, reflects our culture and history, and connects us to others. By learning a new language, we can broaden our horizons, deepen our understanding of different cultures, and build bridges between people. It's never too late to start learning a new language, and the benefits are endless. Can you type this passage about the importance of language without any typos?`,
+`Creativity is the spark that ignites innovation and change. It's the ability to see the world in new and unique ways, and to bring fresh ideas and perspectives to the table. However, creativity is not just for artists and inventors. Anyone can cultivate their creativity by exploring new hobbies, taking risks, and thinking outside the box. By embracing our creativity, we can solve problems, make a difference, and lead fulfilling lives. Can you type this passage about the power of creativity without any mistakes?`
+]
+
+const typingText = document.querySelector(`.typing-text p`);
+inpField = document.querySelector(".wrapper .input-field");
+let charIndex = mistakes = isTyping = 0;
+mistakeTag = document.querySelector(".mistake span");
+timeTag = document.querySelector(".time span b");
+cpmTag = document.querySelector(".cpm span");
+wpmTag = document.querySelector(".wpm span");
+tryAgain = document.querySelector(" button");
+sult = document.querySelector(".sult");
+game = document.querySelector(".wrapper");
+mis = document.querySelector(".mis");
+ccc = document.querySelector(".ccc");
+www = document.querySelector(".www");
+maxt = 60;
+s = maxt;
+let char = inde = 0;
+inde++
+
+function randomParagraph(){
+    let randomnum = Math.floor(Math.random() * paragraph.length)
+    typingText.innerHTML = "";
+
+    paragraph[randomnum].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    })
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping(){
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if(!isTyping){
+        timer = setInterval(initTimer, 1000);
+        isTyping = true;
+    }
+     if(charIndex < characters.length - 1 && s > 0){
+        if(typedChar == null){
+            charIndex--;
+            inde--;
+            
+            if(characters[charIndex].classList.contains("incorrect")){
+                mistakes--;
+            }
+            characters[charIndex].classList.remove("incorrect", "correct")
+        }else{
+            if(characters[charIndex].innerText === typedChar){
+                characters[charIndex].classList.add("correct");
+                
+        
+            }else{
+                characters[charIndex].classList.add("incorrect");
+                mistakes++
+                
+            }
+            charIndex++
+            inde++
+        }
+        characters.forEach(span => span.classList.remove('active') )
+        characters[charIndex].classList.add("active")
+
+        char = characters.length
+    
+        let wpm = Math.round((((charIndex - mistakes) / 5)/ (maxt - s)) * 60)
+        mistakeTag.innerText =  mistakes;
+        cpmTag.innerText = " " + charIndex - mistakes;
+        wpmTag.innerText =  wpm;
+        
+    }else{
+        inpField.value = "";
+        mis.innerText =mistakes;
+        ccc.innerText = charIndex - mistakes;
+        www.innerText = wpmTag.innerText;
+    }
+    
+    
+}
+
+
+function initTimer(){
+    if (inde == char){
+        clearInterval(timer)
+        sult.style.display = "flex";
+        game.style.display = "none";
+    }else if(s > 0){
+        s--;
+        timeTag.innerText = s + `s`;
+        
+    }else{
+        clearInterval(timer);
+        sult.style.display = "flex";
+        game.style.display = "none";
+        
+    }}
+    
+
+
+function resetGame(){
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer)
+    s = maxt;
+    charIndex = mistakes = isTyping = 0;
+    timeTag.innerText = s;
+    mistakeTag.innerText = mistakes;
+        cpmTag.innerText = 0;
+        wpmTag.innerText= 0;
+        inde = 1;
+        sult.style.display = "none";
+        game.style.display = "flex";
+}
+
+   
+randomParagraph()
+inpField.addEventListener("input", initTyping);
+tryAgain.addEventListener("click", resetGame);
+ 
